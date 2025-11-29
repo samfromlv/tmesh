4e7a6543e83c62f23b3eb0bf11cd7eeef559f1b2
Alexander Shakhov <samfromlv@gmail.com>
2025-11-29 12:11:36 +0300

    Added manuals

---
+++ TMesh/mosquitto_add_user.sh
@@ -0,0 +1,6 @@
+﻿sudo docker exec -it mosquitto sh
+##################################################
+# inside container:
+cd /mosquitto/config
+#!aaaaaa your node id (with !), you will be asked to set password
+mosquitto_passwd ./pwd !aaaaaaa
