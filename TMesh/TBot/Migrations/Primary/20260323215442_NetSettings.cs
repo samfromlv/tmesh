c23413aa68287239f6c334725c7d5d2464002d66
Alexander Shakhov <samfromlv@gmail.com>
2026-03-24 00:55:23 +0300

    Added migration

---
+++ TMesh/TBot/Migrations/Primary/20260323215442_NetSettings.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TBot.Migrations.Primary
+{
+    /// <inheritdoc />
+    public partial class NetSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DisablePongs",
+                table: "Networks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Networks",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisablePongs",
+                table: "Networks");
+
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Networks");
+        }
+    }
+}
