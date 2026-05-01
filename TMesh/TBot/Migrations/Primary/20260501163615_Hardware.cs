fa8e0a3a01bd49d9027a3565556be7501febd474
Alexander Shakhov <samfromlv@gmail.com>
2026-05-01 19:37:07 +0300

    Added DB migration

---
+++ TMesh/TBot/Migrations/Primary/20260501163615_Hardware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TBot.Migrations.Primary
+{
+    /// <inheritdoc />
+    public partial class Hardware : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HardwareModel",
+                table: "Devices",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "MacAddress",
+                table: "Devices",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HardwareModel",
+                table: "Devices");
+
+            migrationBuilder.DropColumn(
+                name: "MacAddress",
+                table: "Devices");
+        }
+    }
+}
